a6f0292eebd1bd70b2a69b1681eef7d5421ff06f
Swayam gupta <113286968+swayam0-1@users.noreply.github.com>
2023-09-10 22:25:20 +0530

    Add files via upload

---
+++ todo_list/lib/model/todo.dart
@@ -0,0 +1,22 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true ),
+      ToDo(id: '02', todoText: 'breakfast', isDone: true ),
+      ToDo(id: '03', todoText: 'Check Emails', ),
+      ToDo(id: '04', todoText: 'Team Meeting', ),
+      ToDo(id: '05', todoText: 'workplace ', ),
+      ToDo(id: '06', todoText: 'Dinner with mines', ),
+    ];
+  }
+}
